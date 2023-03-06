36546d1a19d926bc9b527a0cf875b6c807864d7e
“fishplateman” <android9s@icloud.com>
2023-03-05 20:44:56 -0500

    test

--- script/script.sh
+++ script/script.sh
@@ -1,55 +1,60 @@
-#ps -ef | grep yum | grep -v grep | awk '{print $2}' | xargs sudo kill -9
 #!/bin/bash
-#Pre-requisites: Install JDK, Mysql Server
-# echo "installing maven"
-# wget http://mirrors.estointernet.in/apache/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz
-# echo "unzipping maven"
-# tar -xzvf apache-maven-3.6.3-bin.tar.gz
-# sudo rm -f   /var/run/yum.pid
-sudo yum update -y 
 echo "updating yum..."
-sudo yum update
+sudo yum update -y
 echo "upgrading yum..."
 sudo yum upgrade -y
 echo "cleaning yum..."
 sudo yum clean all
 echo "remove jdk..."
-sudo yum remove -y java
+sudo rpm -qa | grep -i java | xargs -n1 rpm -e --nodeps
 echo "installing jdk..."
 sudo yum install -y java-1.8.0-openjdk
 echo "installing nginx"
-sudo yum install nginx -y
-echo "Installing Mysql Server..."
-sudo yum install mysql-server
-echo "Installing start Mysql Server..."
+sudo amazon-linux-extras install nginx1.12
+echo "Installing MySQL server..."
+sudo yum remove mysql-server mysql
+sudo rm -rf /var/lib/mysql
+sudo rm -rf /etc/mysql
+sudo rm -rf /var/log/mysql
+sudo deluser -r mysql
+sudo rm /etc/apparmor.d/abstractions/mysql
+sudo yum install https://dev.mysql.com/get/mysql80-community-release-el7-5.noarch.rpm -y
+sudo yum install mysql-community-server -y
+echo "Starting the MySQL service..."
 sudo systemctl start mysqld
-echo "Installing git..."
-sudo yum install git -y
+sudo rm -rf /var/lib/mysql
+sudo systemctl restart mysqld
 echo "Installing maven..."
 sudo yum install maven -y
+echo "Installing git..."
+sudo yum install git -y
+echo "Installing expect..."
+sudo yum install -y tcl
+sudo yum install -y expect
 
 
-#Set Env Variables
-# echo "Setting JAVA_HOME env variable..."
-# export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
-# echo "Setting MYSQL_HOME env variable"
-# export MYSQL_HOME=/var/lib/mysql 
-# Configure MySQL server
 echo "Configure MySQL server..."
-mysql -u root -p 
-# Change password
-echo "Change password..." 
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY 'aabcd';
-FLUSH PRIVILEGES; 
-EXIT
-#Download Spring Boot project from github
-echo "Downloading webapp from Github..."
-git clone git@github.com:AlatusKitty/webapp.git
-#build the project
-echo "Building the project"
-cd webapp
-mvn clean install 
-#deploy the application
-echo "Deploying the application"
-mvn package
-mvn spring-boot:run 
+password=`sudo grep 'temporary password' /var/log/mysqld.log`
+psw="${password##*localhost: }"
+echo $psw
+
+expect -c "
+    spawn mysql -u root -p
+    expect \"Enter password: \"
+    send \"${psw}\r\"
+    expect \"mysql> \"
+    send \"ALTER USER 'root'@'localhost' IDENTIFIED BY 'Password123#@!'; \r\"
+    expect \"mysql> \"
+    send \"SET GLOBAL validate_password.length=4; \r\"
+    expect \"mysql> \"
+    send \"SET GLOBAL validate_password.policy=0; \r\"
+    expect \"mysql> \"
+    send \"ALTER USER 'root'@'localhost' IDENTIFIED BY 'aabcd'; \r\"
+    expect \"mysql> \"
+    send \"FLUSH PRIVILEGES; \r\"
+    expect \"mysql> \"
+    send \"EXIT;\r\"
+    expect eof
+"
+
+
